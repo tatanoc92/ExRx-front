635a01feaa4ad8ddfea86eb20bb131c90e599d10
William <william.hardy.abeloos@gmail.com>
2023-09-06 22:35:24 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,18 @@ export default function HomePage({ featuredProduct = null, newProducts = [] }) {
   );
 }
 
-// export async function getServerSideProps() {
-//   const featuredProductId = "64b67bd6b00474e00bd2a7a0";
-//   await mongooseConnect();
-//   const featuredProduct = await Product.findById(featuredProductId);
-//   const newProducts = await Product.find({}, null, {
-//     sort: { _id: -1 },
-//     limit: 10,
-//   });
-//   return {
-//     props: {
-//       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)) || {},
-//       newProducts: JSON.parse(JSON.stringify(newProducts)) || [],
-//     },
-//   };
-// }
+export async function getServerSideProps() {
+  const featuredProductId = "64b67bd6b00474e00bd2a7a0";
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)) || {},
+      newProducts: JSON.parse(JSON.stringify(newProducts)) || [],
+    },
+  };
+}
