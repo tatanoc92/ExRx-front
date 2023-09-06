a793a301824f44037d74a548f8629f208d174afe
PierreTTT <pierre.tran.99@gmail.com>
2023-09-06 16:31:14 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
-import { Product } from "@/models/Product";
-import { mongooseConnect } from "@/lib/mongoose";
 import NewProducts from "@/components/NewProducts";
 
 export default function HomePage({ featuredProduct = null, newProducts = [] }) {
